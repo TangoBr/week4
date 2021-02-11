b6db7166d62291d3fbd837b37a3c30d8b04bce76
TangoBr <thomasbridges01@gmail.com>
2021-02-11 18:04:13 -0500

    homeowork done, need better understanding of parse

---
+++ app1.js
@@ -0,0 +1,41 @@
+console.log("Hello World");
+console.warn("We lit!");
+console.error("System critical!");
+
+var1 = ("bowser");
+// var1=(`bowser`);
+console.log(var1);
+
+console.log(`
+
+`);
+
+var2 = 24;
+console.log(var2);
+
+console.log(var1 + var2); //I figured it would be bowser24
+
+/* log,
+warning,
+error,
+int, 
+string,
+math, 
+*/
+
+console.log("string"); //not sure what im guessing here, it's certainly a string
+
+var3 = "stringy text";
+console.log(var3);
+
+var1 =5 ;
+var2 ="5";
+
+parseInt(var2);
+console.log(typeof var2);
+
+var var5 = var1 + var2;
+console.log(var5);
+
+
+
