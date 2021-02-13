135e1ece22e8335c41f5cfac6a779dae5881ecbe
TangoBr <thomasbridges01@gmail.com>
2021-02-13 13:47:59 -0500

    Didn't work

--- app2.js
+++ app2.js
@@ -8,13 +8,13 @@ Math.pow(var1,var2) = var3;
 console.log(var3);
 alert();
 
-var num3 = Math.pow(num1,num2);
+var var3 = Math.pow(var1,var2);
 
-alert(num3);
+alert(var3);
 
-var num4 = Math.floor((Math.random() * 3) +1);
+var var4 = Math.floor((Math.random() * 3) +1);
 
-console.log(num4);
+console.log(var4);
 
 var alphabet = ["a","b","c","d","e","f",];
 
