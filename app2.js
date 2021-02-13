f2e643c370436c199f0a670a6e43fc1c1eeb6e46
TangoBr <thomasbridges01@gmail.com>
2021-02-13 13:43:38 -0500

    cheated but achieved

---
+++ app2.js
@@ -0,0 +1,29 @@
+var var1 = window.prompt("Enter a number");
+var var2 = window.prompt("Enter another number");
+alert();
+console.log(Math.random(1,10));
+var alphabet = ["a", "b", "c", "d", "e", "f"];
+console.log(alphabet);
+Math.pow(var1,var2) = var3;
+console.log(var3);
+alert();
+
+var num3 = Math.pow(num1,num2);
+
+alert(num3);
+
+var num4 = Math.floor((Math.random() * 3) +1);
+
+console.log(num4);
+
+var alphabet = ["a","b","c","d","e","f",];
+
+alphabet.pop("f");
+
+alphabet.push("g");
+
+alphabet.shift("a");
+
+alphabet.unshift("1");
+
+console.log(alphabet);
