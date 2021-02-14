42ad1745c230dc6966f14719541b6254f877892b
TangoBr <thomasbridges01@gmail.com>
2021-02-14 13:02:46 -0500

    learning

--- app2.js
+++ app2.js
@@ -1,29 +1,35 @@
 var var1 = window.prompt("Enter a number");
 var var2 = window.prompt("Enter another number");
-alert();
-console.log(Math.random(1,10));
+// alert();
+console.log(Math.random(1,3)+1);
 var alphabet = ["a", "b", "c", "d", "e", "f"];
 console.log(alphabet);
-Math.pow(var1,var2) = var3;
-console.log(var3);
-alert();
-
 var var3 = Math.pow(var1,var2);
-
+console.log(var3);
 alert(var3);
-
-var var4 = Math.floor((Math.random() * 3) +1);
-
-console.log(var4);
-
-var alphabet = ["a","b","c","d","e","f",];
-
 alphabet.pop("f");
-
 alphabet.push("g");
-
 alphabet.shift("a");
-
 alphabet.unshift("1");
-
 console.log(alphabet);
+
+// alphabet.push("g");
+
+// var var4 = math.floor((math.random()* 3)+1)
+// console.log(var4)
+
+// var var4 = Math.floor((Math.random() * 3) +1);
+
+// console.log(var4);
+
+// var alphabet = ["a","b","c","d","e","f",];
+
+// alphabet.pop("f");
+
+// alphabet.push("g");
+
+// alphabet.shift("a");
+
+// alphabet.unshift("1");
+
+// console.log(alphabet);
